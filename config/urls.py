45c5cd6202bf3a0f688ceacd555bd68eb79a5eed
alff0x1f <alff0x1f@gmail.com>
2026-05-05 14:13:56 +0500

    feat: update base templates

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/contributors/"), name="home"),
     path("admin/", admin.site.urls),
     path("demo/", include("apps.demo.urls")),
     path("contributors/", include("apps.contributors.urls")),
